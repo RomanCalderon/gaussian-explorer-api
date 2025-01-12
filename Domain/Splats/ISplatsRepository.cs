7af1015d9404d5a634ba5e4dfa4e89038ac3a634
Roman Calderon <romanc647@gmail.com>
2025-01-12 14:56:25 -0500

    Add ISplatsRepository interface for managing splat data

---
+++ Domain/Splats/ISplatsRepository.cs
@@ -0,0 +1,10 @@
+﻿namespace Domain.Splats;
+
+public interface ISplatsRepository
+{
+    Task<Splat?> GetAsync(int id, CancellationToken ct);
+    Task<SplatCollection> GetByUserIdAsync(int userId, CancellationToken ct);
+    Task AddAsync(Splat splat, CancellationToken ct);
+    Task UpdateAsync(Splat splat, CancellationToken ct);
+    Task RemoveAsync(Splat splat, CancellationToken ct);
+}
---
+++ Domain/Splats/SplatCollection.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+
+namespace Domain.Splats;
+
+public sealed record SplatCollection(IEnumerable<Splat> Splats) : IReadOnlyCollection<Splat>
+{
+    private readonly List<Splat> _splats = new(Splats);
+
+    public int Count => _splats.Count;
+
+    public IEnumerator<Splat> GetEnumerator() => _splats.GetEnumerator();
+
+    IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+}

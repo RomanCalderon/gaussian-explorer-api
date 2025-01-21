958ecacb4e465eb03a4f9d8ba3d8e67619bdad11
Roman Calderon <romanc647@gmail.com>
2025-01-21 17:50:02 -0500

    Add identity column to Posts table in database migration

---
+++ Persistence/Migrations/20250121205810_AddIdentityToPostId.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddIdentityToPostId : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Post",
+                table: "Posts");
+
+            migrationBuilder.DropColumn(
+                name: "Id",
+                table: "Posts");
+
+            migrationBuilder.AddColumn<int>(
+                name: "Id",
+                table: "Posts",
+                nullable: false,
+                defaultValue: 0)
+                .Annotation("SqlServer:Identity", "1, 1");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Id",
+                table: "Posts");
+
+            migrationBuilder.AddColumn<int>(
+                name: "Id",
+                table: "Posts",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Post",
+                table: "Posts",
+                column: "Id");
+        }
+    }
+}

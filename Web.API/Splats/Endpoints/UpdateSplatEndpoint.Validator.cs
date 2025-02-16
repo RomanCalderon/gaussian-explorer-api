d57763b6249c571170ab447990192be803826ba8
Roman Calderon <romanc647@gmail.com>
2025-02-15 21:53:16 -0500

    Feat: Update Splat validation logic to support partial updates

--- Web.API/Splats/Endpoints/UpdateSplatEndpoint.Validator.cs
+++ Web.API/Splats/Endpoints/UpdateSplatEndpoint.Validator.cs
@@ -8,8 +8,20 @@ public class UpdateSplatEndpoint : Validator<UpdateSplatRequest>
 {
     public UpdateSplatEndpoint()
     {
-        RuleFor(x => x.Title).NotEmpty();
-        RuleFor(x => x.Description).NotEmpty();
-        RuleFor(x => x.Url).NotEmpty();
+        RuleFor(x => x.Title)
+            .NotEmpty()
+            .When(x => x.Title is not null);
+
+        RuleFor(x => x.Description)
+            .NotEmpty()
+            .When(x => x.Description is not null);
+
+        RuleFor(x => x.Url)
+            .NotEmpty()
+            .When(x => x.Url is not null);
+
+        RuleFor(x => x.ViewInfo)
+            .NotEmpty()
+            .When(x => x.ViewInfo is not null);
     }
 }
